9755c1df9bb698cd6029af3885f8aab3345eeed9
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-05 22:17:52 +0545

    Fix for __dirname with ES Modules

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import serveFavicon from "serve-favicon"; // Import serve-favicon middleware
 import viewRoutes from "./routes/viewRoutes.js";
 
 dotenv.config();
@@ -13,17 +12,11 @@ const app = express();
 // Fix for __dirname with ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve favicon
-app.use(serveFavicon(path.join(__dirname, "public", "favicon.ico")));
-
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Set view engine
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 // Routes
