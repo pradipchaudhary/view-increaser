e25cf1268f9753be78872d1b852f05857712d890
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-05 22:06:06 +0545

    fixed UI and small bug, ready to deploy

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import schedule from "node-schedule";
+import path from "path";
+import { fileURLToPath } from "url";
 import viewRoutes from "./routes/viewRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+// Fix for __dirname with ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 // Routes
 app.use("/", viewRoutes);
-
-// Schedule task to generate a random URL and hit it every minute
-// schedule.scheduleJob("*/1 * * * *", () => {
-//     const randomUrl = generateRandomUrl();
-//     addUrl(randomUrl);
-//     hitUrl(randomUrl);
-//     console.log(`Generated and hit URL: ${randomUrl}`);
-// });
 
 // Start server
 const PORT = process.env.PORT || 8000;
